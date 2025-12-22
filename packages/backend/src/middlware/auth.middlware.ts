fd9758f4b2c5a94454d7004894e8268f5c524fc2
Oleksandr Bozhko <37217530+ChikiBum@users.noreply.github.com>
2025-12-22 21:10:38 +0200

    fixed auth middleware

--- packages/backend/src/middlware/auth.middlware.ts
+++ packages/backend/src/middlware/auth.middlware.ts
@@ -1,4 +1,4 @@
-export const authMiddleware = (_req, _res, next) => {
+export const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
